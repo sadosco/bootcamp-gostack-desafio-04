394c6c1b9879977d60060a18e675f72e647a6e4c
Sadosco <sadoscolima@gmail.com>
2020-04-20 15:21:29 -0300

    init project

---
+++ front/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="https://i.imgur.com/KDIDiSE.png" alt="facebook-logo" />
+        <span>Meu perfil</span>
+        <i className="material-icons">account_circle</i>
+      </nav>
+    </header>
+  );
+}
